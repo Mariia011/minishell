05a187da484252c2ee3dfbfcac1a2466d7919365
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-09-26 01:53:31 +0400

    making ast

--- src/ast/ast_eval.c
+++ src/ast/ast_eval.c
@@ -6,60 +6,60 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 01:43:14 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/09/25 23:15:21 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/09/26 01:46:41 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int dfs(t_ast_node *root, t_ast *ast);
+static int dfs(t_ast_node *root, t_ast *ast, int stdout);
 
 void ast_eval(t_ast *ast)
 {
-	dfs(ast->root, ast);
+	dfs(ast->root, ast, ast->shell->stddesc->stdout);
 
 	dup2(ast->shell->stddesc->stdin, STDIN_FILENO);
 	dup2(ast->shell->stddesc->stdout, STDOUT_FILENO);
 	dup2(ast->shell->stddesc->stderr, STDERR_FILENO);
 }
 
-static int dfs(t_ast_node *root, t_ast *ast)
+static int dfs(t_ast_node *root, t_ast *ast, int stdout)
 {
-	if (!root) return 1;
+	if  (!root) return 1;
 
 	if (root->type == AND)
 	{
-		return (dfs(root->left, ast) && dfs(root->right, ast));
+		return (dfs(root->left, ast, stdout) && dfs(root->right, ast, stdout));
 	}
 	else if (root->type == OR)
 	{
-		return (dfs(root->left, ast) || dfs(root->right, ast));
+		return (dfs(root->left, ast, stdout) || dfs(root->right, ast, stdout));
 	}
 	else if (root->type == PIPE)
 	{
+
 		t_fd	pipes[PIPE_MAX];
 
 		pipe(pipes);
 
 		dup2(pipes[out], STDOUT_FILENO);
 
-		dup2(pipes[in], STDIN_FILENO);
+		dfs(root->left, ast, pipes[out]);
 
-		dfs(root->left, ast);
+		dup2(pipes[in], STDIN_FILENO);
 
 		close(pipes[in]);
 		close(pipes[out]);
 
-		if (!root->p || root->p->type != PIPE)
-			dup2(ast->shell->stddesc->stdout, STDOUT_FILENO);
+		dup2(stdout, STDOUT_FILENO);
 
-		return dfs(root->right, ast);
+		return dfs(root->right, ast, stdout);
 	}
 	else if (root->type == CMD)
 	{
 		root->cmd_ptr->eval(root->cmd_ptr);
 
-		if (!root->p || root->p->type == PIPE || is_program(root->cmd_ptr))
+		if (root == ast->last_process_cmd || root == ast->last_cmd || !root->p)
 			while (-1 != wait(NULL));
 
 		return !get_exit_status();
