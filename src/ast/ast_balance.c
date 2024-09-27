dcff6da0f2baf2cb0c92bc1f499612551aea276e
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-09-27 21:57:06 +0400

    nice

--- src/ast/ast_balance.c
+++ src/ast/ast_balance.c
@@ -6,13 +6,13 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 02:39:14 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/09/26 04:12:38 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/09/27 21:53:14 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void ast_balance(t_ast *ast, t_list *tokens) // ls && ((echo a | cat -e) && pwd)
+void ast_balance(t_ast *ast, t_list *tokens) // echo a && (echo d || (echo b && echo c))
 {
 	t_node *p = shfind_if(tokens->head, tokens->tail, is_opening_parenthesis_token, ast->shell);
 
@@ -25,7 +25,7 @@ void ast_balance(t_ast *ast, t_list *tokens) // ls && ((echo a | cat -e) && pwd)
 		t_ast_node *z = ast_find_by_token(ast, shrfind_if(tokens->head, p->prev, is_special_symbol, ast->shell));
 
 		ast_rrotate(ast, x, z);
-		p = shfind_if(n->next, tokens->tail, is_opening_parenthesis_token, ast->shell);
+		p = shfind_if(p->next, tokens->tail, is_opening_parenthesis_token, ast->shell);
 	}
 
 }
