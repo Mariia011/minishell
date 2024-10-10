d104f2f3472e4c65c6b1720c711cd5c8a00b0fd7
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-10-10 22:18:54 +0400

    .

--- src/name_lookup/redirect.c
+++ src/name_lookup/redirect.c
@@ -6,7 +6,7 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 22:07:40 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/10/10 20:54:14 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/10/10 22:18:04 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,14 @@ static int	process_outfile(t_ast_node *r);
 static int	process_append(t_ast_node *r);
 static int	process_heredoc(t_ast_node *r, t_shell *shell);
 
+void __redirect_out__(t_authorized_fds *newfds, t_ast_node *r);
+void __redirect_in__(t_authorized_fds *newfds, t_ast_node *r);
+
 t_authorized_fds	redirect(t_ast_node *r, t_authorized_fds oldfds)
 {
 	t_authorized_fds newfds = oldfds;
 	if (r->redirection_type & (redirect_in | redirect_heredoc))
-	{
-		dup2(r->ast->shell->stddesc->stdin, STDIN_FILENO);
-
-		if (r->redirection_type & redirect_in)
-			newfds.stdin.fd = (process_infile(r));
-		else
-			newfds.stdin.fd = (process_heredoc(r, r->ast->shell));
-		newfds.stdin.author = r;
-
-		dup2(newfds.stdin.fd, STDIN_FILENO);
-		if (newfds.stdin.fd == -1 && (r->redirection_type & redirect_in))
-		{
-			if (find_addr(r->ast->shell->dollar_tokens, r->right->orig_token))
-				__va_perror(get_orig_val(r->right->orig_token, r->ast->shell), ": ambiguous redirect", NULL);
-			else if (string_equal(r->right->filename, "*") && !find_addr(r->ast->shell->quoted_tokens, r->right->orig_token))
-				__va_perror("*: ambiguous redirect", NULL);
-			else
-				__va_perror(r->right->filename, ": ", "no such file or directory", NULL);
-		}
-		r->right->fd = newfds.stdin.fd;
-	}
+		__redirect_in__(&newfds, r);
 	else if (r->redirection_type & (redirect_out | redirect_append))
 	{
 		if (string_equal(r->right->filename, "*") && !find_addr(r->ast->shell->quoted_tokens, r->right->orig_token))
@@ -51,23 +34,51 @@ t_authorized_fds	redirect(t_ast_node *r, t_authorized_fds oldfds)
 			r->right->fd = -1;
 			return newfds;
 		}
-		if (r->redirection_type & redirect_out)
-			newfds.stdout.fd = (process_outfile(r));
-		else
-			newfds.stdout.fd = (process_append(r));
-		newfds.stdout.author = r;
-		dup2(newfds.stdout.fd, STDOUT_FILENO);
-		if (newfds.stdout.fd == -1)
-		{
-			if (find_addr(r->ast->shell->dollar_tokens, r->right->orig_token))
-				__va_perror(get_orig_val(r->right->orig_token, r->ast->shell), ": ambiguous redirect", NULL);
-			else
-				__va_perror(r->right->filename, ": ", "could not open file", NULL);
-		}
-		r->right->fd = newfds.stdout.fd;
+		__redirect_out__(&newfds, r);
 	}
 
 	return newfds;
+}
+
+void __redirect_out__(t_authorized_fds *newfds, t_ast_node *r)
+{
+	if (r->redirection_type & redirect_out)
+		newfds->stdout.fd = (process_outfile(r));
+	else
+		newfds->stdout.fd = (process_append(r));
+	newfds->stdout.author = r;
+	dup2(newfds->stdout.fd, STDOUT_FILENO);
+	if (newfds->stdout.fd == -1)
+	{
+		if (find_addr(r->ast->shell->dollar_tokens, r->right->orig_token))
+			__va_perror(get_orig_val(r->right->orig_token, r->ast->shell), ": ambiguous redirect", NULL);
+		else
+			__va_perror(r->right->filename, ": ", "could not open file", NULL);
+	}
+	r->right->fd = newfds->stdout.fd;
+}
+
+void __redirect_in__(t_authorized_fds *newfds, t_ast_node *r)
+{
+	dup2(r->ast->shell->stddesc->stdin, STDIN_FILENO);
+
+	if (r->redirection_type & redirect_in)
+		newfds->stdin.fd = (process_infile(r));
+	else
+		newfds->stdin.fd = (process_heredoc(r, r->ast->shell));
+	newfds->stdin.author = r;
+
+	dup2(newfds->stdin.fd, STDIN_FILENO);
+	if (newfds->stdin.fd == -1 && (r->redirection_type & redirect_in))
+	{
+		if (find_addr(r->ast->shell->dollar_tokens, r->right->orig_token))
+			__va_perror(get_orig_val(r->right->orig_token, r->ast->shell), ": ambiguous redirect", NULL);
+		else if (string_equal(r->right->filename, "*") && !find_addr(r->ast->shell->quoted_tokens, r->right->orig_token))
+			__va_perror("*: ambiguous redirect", NULL);
+		else
+			__va_perror(r->right->filename, ": ", "no such file or directory", NULL);
+	}
+	r->right->fd = newfds->stdin.fd;
 }
 
 static int	process_infile(t_ast_node *r)
